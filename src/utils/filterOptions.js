ee182df2494124ff39793cddc2627a6b72bb0e96
Alex Burke <10841351+SupremacyXI@users.noreply.github.com>
2024-06-11 20:48:57 -0700

    Update filterOptions.js

--- src/utils/filterOptions.js
+++ src/utils/filterOptions.js
@@ -3,8 +3,8 @@ const filterOptions = {
     model: ['M3', 'R8', 'GT350'],
     year: ['2025', '2024', '2023', '2022', '2021', '2020', '2019', '2018', '2017'],
     status: ['clean', 'Rebuilt', 'Salvage'],
-    exterior: ['black', 'white', 'blue', 'grey', 'red', 'orange', 'yellow', 'green', 'other'],
-    interior: ['black', 'white', 'blue', 'grey', 'red', 'orange', 'yellow', 'green', 'other'],
+    exterior: ['black', 'white', 'blue', 'grey', 'red', 'orange', 'yellow', 'green', 'biege', 'brown'],
+    interior: ['black', 'white', 'blue', 'grey', 'red', 'orange', 'yellow', 'green', 'biege', 'brown'],
     seats: ['competition', 'base', 'recaro', 'sparco'],
     transmission: ['DCT', 'Manual', 'ZF8', 'Automatic']
 };
